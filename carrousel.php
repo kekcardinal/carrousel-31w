f126617b9095cace6f9b238aea9c8f01ff675a7c
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2023-03-08 10:53:38 -0500

    c11 début du carrousel

---
+++ carrousel.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Extension carrousel permet d'afficher dans une boite modale les images d'une galerie
+ * Package name : Carrousel
+ * Version: 1.0.0
+ */
+
+ /**
+  * Plugin name: Carrousel
+  * Author: Felix Boutin-Cousineau
+  * Plugin URI: https:github.com/kekcardinal/carrousel
+  * Description: Carrousel qui permet d'afficher dans une boite modale les images d'une galerie avec un système de navigation
+  */
