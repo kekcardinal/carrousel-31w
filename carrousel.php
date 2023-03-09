d8f7d771c903054bd5f4f564e437fb28dfb67b51
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2023-03-09 14:51:11 -0500

    C12 carrousel.php enqueue génération du code HTML

--- carrousel.php
+++ carrousel.php
@@ -12,3 +12,37 @@
   * Plugin URI: https:github.com/kekcardinal/carrousel
   * Description: Carrousel qui permet d'afficher dans une boite modale les images d'une galerie avec un système de navigation
   */
+
+function carrousel_enqueur(){
+    $version_css = filemtime(plugin_dir_path(__FILE__) . "style.css");
+    $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
+
+    wp_enqueue_style(   'em_plugin_carrousel_css',
+    plugin_dir_url(__FILE__) . "style.css",
+    array(),
+    $version_css);
+
+wp_enqueue_script(  'em_plugin_carrousel_js',
+   plugin_dir_url(__FILE__) ."js/carrousel.js",
+   array(),
+   $version_js,
+   true);
+
+
+}
+ 
+add_action('wp_enqueue_scripts','carrousel_enqueue');
+
+function creation_carrousel(){
+
+    return '
+    <button class="bouton__ouvrir">Ouvrir</button>
+    <div class="carrousel">
+    <button class="bouton__x">X</button>
+    <figure class="carrousel__figure"></figure>
+    <form class="carrousel__form"></form>
+    </div>';
+}
+
+add_shortcode('carrousel', 'creation_carrousel');
+
